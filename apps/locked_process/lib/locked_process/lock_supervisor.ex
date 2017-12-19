fe4537953baa43f47f93b3a23d8eb3569f2c05fa
Rob Keefer <rob.keefer@pomiet.com>
2017-12-18 22:41:45 -0500

    Update supervisor tests

--- apps/locked_process/lib/locked_process/lock_supervisor.ex
+++ apps/locked_process/lib/locked_process/lock_supervisor.ex
@@ -2,20 +2,15 @@ defmodule LockSupervisor do
   use Supervisor
 
   def start_link([combination, message, delay]) do
+    {:ok, pid} = Supervisor.start_link(__MODULE__, [combination, message, delay])
+  end
+
+  def init([combination, message, delay]) do
     children = [
       worker(LockedProcess, [combination, message, delay])
     ]
     opts = [strategy: :one_for_one, name: LockedProcess.Supervisor]
 
-    {:ok, _pid} = Supervisor.start_link(children, opts)
-  end
-
-  def init(:ok) do
-    children = [
-      worker(LockedProcess, [123, "default message", 1])
-    ]
-    opts = [strategy: :one_for_one, name: LockedProcess.Supervisor]
-
-    Supervisor.start_link(children, opts)
+    supervise(children, opts)
   end
 end
--- apps/locked_process/test/locked_process/lock_supervisor_test.exs
+++ apps/locked_process/test/locked_process/lock_supervisor_test.exs
@@ -1,26 +1,16 @@
 defmodule LockedProcess.SupervisorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   setup do
     {:ok, server_pid} = LockSupervisor.start_link([123, "Correct: E", 1])
     {:ok, server: server_pid}
   end
 
-  test "try default combination returns ok" do
+  test "supervisor restarts after exit", %{server: pid} do
+    assert {:ok, "Correct: E"} == LockedProcess.pick_lock(123)
+    Process.unlink(pid)
+    Process.exit(pid, :shutdown)
+    :timer.sleep 1000
     assert {:ok, "Correct: E"} == LockedProcess.pick_lock(123)
   end
-
-  test "attempt with 2 returns error" do
-    assert {:error, "Can't crack me!"} == LockedProcess.pick_lock(2)
-  end
-
-  test "reset combination success" do
-    assert {:ok} == LockedProcess.reset({123, [456, "Correct: L"]})
-    assert {:ok, "Correct: L"} == LockedProcess.pick_lock(456)
-  end
-
-  test "reset combination failure" do
-    assert {:error,"Can't crack me!"} == LockedProcess.reset({456, [789, "Correct: L"]})
-  end
-
 end
